8a77b9ba381a91530a43c91a6075650ba69bbf84
Jonas Obrist <ojiidotch@gmail.com>
2012-01-28 01:52:34 +0100

    added non-PLTE png minor speedups in resizers.py

--- notpil/resizers.py
+++ notpil/resizers.py
@@ -5,12 +5,18 @@ import array
 
 def nearest(source, width, height, pixelsize):
     assert pixelsize == 1, "yea... gotta implement this generically"
-    pixels = [array.array('B', [0 for _ in  range(width)]) for _ in range(height)]
-    x_ratio = fdiv(source.width, width)
-    y_ratio = fdiv(source.height, height)
-    for y, line in enumerate(pixels):
-        source_y = int(round(y * y_ratio))
-        for x, _ in enumerate(line):
-            source_x = int(round(x * x_ratio))
-            pixels[y][x] = source.pixels[source_y][source_x]
+    pixels = []
+    pixelappend = pixels.append # cache for cpython
+    x_ratio = fdiv(source.width, width) # get the x-axis ratio
+    y_ratio = fdiv(source.height, height) # get the y-axis ratio
+    yrange = range(height) # an iterator over the indices of all lines (y-axis)
+    xrange = range(width) # an iterator over the indices of all rows (x-axis)
+    for y in yrange:
+        source_y = int(round(y * y_ratio)) # get the source line
+        line = array.array('B') # initialize a new line
+        lineappend = line.append # cache for cypthon
+        for x in xrange:
+            source_x = int(round(x * x_ratio)) # get the source row
+            lineappend(source.pixels[source_y][source_x])
+        pixelappend(line)
     return pixels
