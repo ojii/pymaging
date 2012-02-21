e0ccbf7b46a3c41f3917f7d29dff2b8aa1279a7e
Jonas Obrist <ojiidotch@gmail.com>
2012-02-21 11:55:10 +0100

    added coverage support for runtests.py

--- runtests.py
+++ runtests.py
@@ -4,7 +4,26 @@ from unittest.main import TestProgram
 
 class MyTestProgram(TestProgram):
     def parseArgs(self, argv):
-        self._do_discovery([])
+        self._do_discovery(argv[1:])
+
+def main():
+    import optparse
+    parser = optparse.OptionParser()
+    parser.add_option('--coverage', dest='coverage', default=False, action='store_true')
+    options, args = parser.parse_args()
+    if options.coverage:
+        import coverage
+        cov = coverage.coverage(source=['notpil'], branch=True)
+        cov.start()
+    else:
+        cov = None
+    args.insert(0, 'runtests.py')
+    try:
+        MyTestProgram(argv=args)
+    finally:
+        if cov:
+            cov.stop()
+            cov.save()
 
 if __name__ == '__main__':
-    MyTestProgram()
+    main()
