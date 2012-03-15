918b0763837cade0dd0b2c3c1983fbdd18eb0baa
Jonas Obrist <ojiidotch@gmail.com>
2012-03-14 23:11:34 -0700

    removed unused code

--- pymaging/incubator/formats/jpg/raw.py
+++ pymaging/incubator/formats/jpg/raw.py
@@ -534,27 +534,6 @@ class TonyJpegDecoder:
         # The spec says that the values given produce "good" quality, and
         # when divided by 2, "very good" quality.
 
-        std_luminance_quant_tbl = [
-          16,  11,  10,  16,  24,  40,  51,  61,
-          12,  12,  14,  19,  26,  58,  60,  55,
-          14,  13,  16,  24,  40,  57,  69,  56,
-          14,  17,  22,  29,  51,  87,  80,  62,
-          18,  22,  37,  56,  68, 109, 103,  77,
-          24,  35,  55,  64,  81, 104, 113,  92,
-          49,  64,  78,  87, 103, 121, 120, 101,
-          72,  92,  95,  98, 112, 100, 103,  99]
-        std_chrominance_quant_tbl = [
-          17,  18,  24,  47,  99,  99,  99,  99,
-          18,  21,  26,  66,  99,  99,  99,  99,
-          24,  26,  56,  99,  99,  99,  99,  99,
-          47,  66,  99,  99,  99,  99,  99,  99,
-          99,  99,  99,  99,  99,  99,  99,  99,
-          99,  99,  99,  99,  99,  99,  99,  99,
-          99,  99,  99,  99,  99,  99,  99,  99,
-          99,  99,  99,  99,  99,  99,  99,  99]
-
-        # For AA&N IDCT method, divisors are equal to quantization
-        # coefficients scaled by scalefactor[row]*scalefactor[col], where
         #   scalefactor[0] = 1
         #   scalefactor[k] = cos(k*PI/16) * sqrt(2)    for k=1..7
         # We apply a further scale factor of 8.
