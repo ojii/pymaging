ab85c1eb146706ffc02888c8a4bc93870ec9bc14
Craig de Stigter <craig.destigter@koordinates.com>
2012-06-07 12:00:05 +0200

    add failing test case for #21

--- pymaging/tests/test_resampling.py
+++ pymaging/tests/test_resampling.py
@@ -2,8 +2,8 @@ from pymaging.tests.test_basic import PymagingBaseTestCase, image_factory
 from pymaging.webcolors import Red, Green, Blue
 
 
-class ResizeCropTests(PymagingBaseTestCase):
-    def test_resize_nearest_resampling(self):
+class ResizeNearestResamplingTests(PymagingBaseTestCase):
+    def test_resize_nearest_resampling_down(self):
         img = image_factory([
             [Red, Green, Blue],
             [Green, Blue, Red],
@@ -13,4 +13,17 @@ class ResizeCropTests(PymagingBaseTestCase):
         self.assertImage(img, [
             [Red, Blue],
             [Blue, Green],
+        ])
+
+    def test_resize_nearest_resampling_up(self):
+        img = image_factory([
+            [Red, Blue],
+            [Blue, Green],
+        ])
+        img = img.resize(4, 4)
+        self.assertImage(img, [
+            [Red, Red, Blue, Blue],
+            [Red, Red, Blue, Blue],
+            [Blue, Blue, Green, Green],
+            [Blue, Blue, Green, Green],
         ])
