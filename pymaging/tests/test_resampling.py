83da4a30f42344d3005d739adfd98587dd00c9db
Craig de Stigter <craig.destigter@koordinates.com>
2012-06-07 13:28:20 +0200

    add test for nearest resampling with transparent pixels

--- pymaging/tests/test_resampling.py
+++ pymaging/tests/test_resampling.py
@@ -1,9 +1,11 @@
 from pymaging.tests.test_basic import PymagingBaseTestCase, image_factory
+from pymaging.colors import Color
 from pymaging.webcolors import Red, Green, Blue
+from pymaging.resample import bilinear
 
 
 class ResizeNearestResamplingTests(PymagingBaseTestCase):
-    def test_resize_nearest_resampling_down(self):
+    def test_resize_nearest_down(self):
         img = image_factory([
             [Red, Green, Blue],
             [Green, Blue, Red],
@@ -15,7 +17,20 @@ class ResizeNearestResamplingTests(PymagingBaseTestCase):
             [Blue, Green],
         ])
 
-    def test_resize_nearest_resampling_up(self):
+    def test_resize_nearest_down_transparent(self):
+        transparent = Color(255, 255, 255, 0)
+        img = image_factory([
+            [Red, Green, Blue],
+            [Green, Blue, Red],
+            [Blue, Red, transparent],
+        ])
+        img = img.resize(2, 2)
+        self.assertImage(img, [
+            [Red, Blue],
+            [Blue, transparent],
+        ])
+
+    def test_resize_nearest_up(self):
         img = image_factory([
             [Red, Blue],
             [Blue, Green],
